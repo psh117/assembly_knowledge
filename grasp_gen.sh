d9a6afe1c00928bc3d21a1a3c63cf9b415c2cb42
Suhan Park <psh117@snu.ac.kr>
2020-04-29 20:31:56 +0900

    update grasp pose required by the new gripper

--- grasp_gen.sh
+++ grasp_gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rosrun fgpg fgpg config/grasp_options.yaml models/meshes/stefan_bottom.stl
-rosrun fgpg fgpg config/grasp_options.yaml models/meshes/stefan_long_part.stl
-rosrun fgpg fgpg config/grasp_options.yaml models/meshes/stefan_middle_part.stl
-rosrun fgpg fgpg config/grasp_options.yaml models/meshes/stefan_short_part.stl
-rosrun fgpg fgpg config/grasp_options.yaml models/meshes/stefan_side_left_part.stl
-rosrun fgpg fgpg config/grasp_options.yaml models/meshes/stefan_side_right_part.stl
+rosrun fgpg fgpg config/grasp_options.yaml models/meshes/ikea_stefan_bottom.stl
+rosrun fgpg fgpg config/grasp_options.yaml models/meshes/ikea_stefan_long.stl
+rosrun fgpg fgpg config/grasp_options_middle.yaml models/meshes/ikea_stefan_middle.stl
+rosrun fgpg fgpg config/grasp_options.yaml models/meshes/ikea_stefan_short.stl
+rosrun fgpg fgpg config/grasp_options.yaml models/meshes/ikea_stefan_side_left.stl
+rosrun fgpg fgpg config/grasp_options.yaml models/meshes/ikea_stefan_side_right.stl
 
 rm models/grasps/*
 mv models/meshes/*_grasp.yaml models/grasps/
